c3dba3f1cdc0b9f8616e54edddb0aa07e54f4b35
Sincenir <843398723@qq.com>
2024-09-12 15:13:15 +0800

    第一章节完成

--- chapter1/main.js
+++ chapter1/main.js
@@ -1,1 +1,14 @@
-console.log('Hello from Electron 👋')
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 200,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
